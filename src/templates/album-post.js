1caa26056ba96f52b1e0612e9ff2229d790aba9d
Derek Vance <derekvance21@gmail.com>
2020-10-20 16:44:51 -0700

    album-post template works locally

--- src/templates/album-post.js
+++ src/templates/album-post.js
@@ -1,12 +1,101 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/Layout'
 
-function AlbumPost () {
-
+const AlbumImages = ({photos}) => {
   return (
-    <div>
-      <h1>Album</h1>
-    </div>
+    photos.map((url) => (
+      <div
+        className="album-image margin-top-0"
+        style={{
+          backgroundImage: `url(${url})`,
+          // backgroundPosition: `top left`,
+          // backgroundAttachment: `fixed`,
+        }}
+      ></div>
+    ))
   )
 }
 
-export default AlbumPost;
+export const AlbumPostTemplate = ({
+  description,
+  photos,
+  title,
+  helmet,
+}) => {
+
+  return (
+    <section className="section">
+      {helmet || ''}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <p>{description}</p>
+            <AlbumImages
+              photos={photos}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+AlbumPostTemplate.propTypes = {
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+  photos: PropTypes.array,
+}
+
+const AlbumPost = ({ data }) => {
+  const { markdownRemark: album } = data
+
+  return (
+    <Layout>
+      <AlbumPostTemplate
+        description={album.frontmatter.description}
+        helmet={
+          <Helmet titleTemplate="%s | Album">
+            <title>{`${album.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${album.frontmatter.description}`}
+            />
+          </Helmet>
+        }
+        // tags={album.frontmatter.tags}
+        title={album.frontmatter.title}
+        photos={album.frontmatter.photos}
+      />
+    </Layout>
+  )
+}
+
+AlbumPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+}
+
+export default AlbumPost
+
+export const pageQuery = graphql`
+  query AlbumPostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+        tags
+        photos
+      }
+    }
+  }
+`
